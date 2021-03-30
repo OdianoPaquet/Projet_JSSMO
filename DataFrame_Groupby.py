4201f84f2cdec4c57892b46071780444fa5721e5
Maxime Descartes MBOGNING <dmbogning15@gmail.com>
2021-03-30 18:37:56 +0200

    conflict on file with Amina

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -59,7 +59,7 @@ print('\n==================================================================\n')
 
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
-sortRatingsField = cloneDF(mergeRatings)
-sortRatingsField = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
+modifiedSortRatingsField = cloneDF(mergeRatings)
+modifiedSortRatingsField = modifiedSortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
     {'COUNT': np.size, 'myAVG': lambda x: x.sum() / float(x.count())}).sort('COUNT', ascending=False)
-print('My info sorted: \n%s' % sortRatingsField[:15])
+print('My info sorted: \n%s' % modifiedSortRatingsField[:15])
